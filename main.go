96e060cc2858ff9863695e4555cece61e0d902ae
Christoffer Back <backchristoffer@hotmail.com>
2024-11-14 14:33:32 +0100

    api added for creating files so I can do this from the browserß

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 // will just create a random value like [1234, 3124, 4312, 4321]
@@ -20,12 +22,12 @@ func randomToken() string {
 }
 
 // will create a new file in /tmp with prefix d1_TIMESTAMP with a random value in each file
-func writeFile(osDir string) {
+func writeFile(osDir string) (string, error) {
 	token := randomToken()
 
 	if token == "" {
 		fmt.Println("Failed to create a random value token")
-		return
+		return "", fmt.Errorf("failed to create token")
 	}
 
 	filename := fmt.Sprintf("%s/d1_%d", osDir, time.Now().UnixNano())
@@ -33,11 +35,23 @@ func writeFile(osDir string) {
 	err := os.WriteFile(filename, []byte(token), 0644)
 	if err != nil {
 		fmt.Println("Error creating file", err)
-		return
+		return "", fmt.Errorf("failed to create file: %v", err)
 	}
-	fmt.Println("File creted: ", filename)
+	fmt.Println("File created: ", filename)
+	return filename, nil
+}
+
+func api_createfile(c *gin.Context) {
+	filename, err := writeFile("/tmp")
+	if err != nil {
+		c.String(500, fmt.Sprintf("Error: %v", err))
+	}
+	c.String(200, fmt.Sprintf("File created at: %s", filename))
 }
 
 func main() {
-	writeFile("/tmp")
+	route := gin.Default()
+	route.GET("/file", api_createfile)
+	route.Run(":8080")
+	fmt.Println("Starting server on 8080")
 }
